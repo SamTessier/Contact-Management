04adfaaf2396fa606885d8cf1f6d82c5d2a73062
SamTessier <samcjtessier@gmail.com>
2023-05-16 23:47:11 -0230

    spacing inputs on loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,6 +3,7 @@ import { AuthContext } from '../AuthContext';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
 
 const LoginForm = ({ onClose }) => {
   const { logIn } = useContext(AuthContext);
@@ -29,30 +30,38 @@ const LoginForm = ({ onClose }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <TextField
-        label="Email"
-        type="email"
-        name="email"
-        required
-        fullWidth
-        autoFocus
-      />
-      <TextField
-        label="Password"
-        type="password"
-        name="password"
-        required
-        fullWidth
-      />
-      {error && <Alert severity="error">{error}</Alert>}
-      <Button type="submit" variant="contained" color="primary" fullWidth>
-        Log In
-      </Button>
-      <Button onClick={onClose} color="secondary" fullWidth>
-        Cancel
-      </Button>
+      <Box mb={2}>
+        <TextField
+          label="Email"
+          type="email"
+          name="email"
+          required
+          fullWidth
+          autoFocus
+        />
+      </Box>
+      <Box mb={2}>
+        <TextField
+          label="Password"
+          type="password"
+          name="password"
+          required
+          fullWidth
+        />
+      </Box>
+      {error && (
+        <Box mb={2}>
+          <Alert severity="error">{error}</Alert>
+        </Box>
+      )}
+      <Box mb={2}>
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Log In
+        </Button>
+      </Box>
     </form>
   );
+  
 }
 
 export default LoginForm;
