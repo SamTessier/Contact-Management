6f8d3278a6c581214a24cd2b12ddf50fff2a5aab
SamTessier <samcjtessier@gmail.com>
2023-07-17 23:33:10 -0230

    strip down vite project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+});
