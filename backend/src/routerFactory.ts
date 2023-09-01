bb5bb206b98e4fab23c2db7833b6a3d9dd8a8452
SamTessier <samcjtessier@gmail.com>
2023-09-01 18:01:35 -0230

    meeting commit

--- backend/src/routerFactory.ts
+++ backend/src/routerFactory.ts
@@ -1,7 +1,10 @@
+// Importing required modules
 import { Router } from "express";
 import mysql from "mysql";
+import { z } from "zod"; // Import Zod for request body validation
 require('dotenv').config();
 
+// Database connection remains the same
 export const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -9,20 +12,13 @@ export const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// User roles and result types
 type Role = "admin" | "teacher" | "student";
-
-type Success = {
-  type: "success";
-  role: Role;
-};
-
-type Failure = {
-  type: "failure";
-  message: string;
-};
-
+type Success = { type: "success"; role: Role };
+type Failure = { type: "failure"; message: string };
 type Result = Success | Failure;
 
+// Async query function, no changes here
 const queryAsync = (sql: string, data: any = []) => {
     return new Promise((resolve, reject) => {
         connection.query(sql, data, (err, results) => {
@@ -32,65 +28,86 @@ const queryAsync = (sql: string, data: any = []) => {
     });
 };
 
+// Modified routerFactory function
+// Added optional custom handler arguments for each CRUD operation
 const routerFactory = (
     table: string,
     {
         userCheck = () => ({ type: "success", role: "student" }),
+        customGetHandler,
+        customPostHandler,
+        customPutHandler,
+        customDeleteHandler
     }: {
         userCheck?: (req: any) => Result;
+        customGetHandler?: Function;
+        customPostHandler?: Function;
+        customPutHandler?: Function;
+        customDeleteHandler?: Function;
     } = {}
 ) => {
     const router = Router();
 
-    router.get("/", async (req, res) => {
-        // const currentUser = req.session.user;
+    // GET Handler, using non-async function
+    router.get("/", (req, res, next) => {
+        if (customGetHandler) {
+            customGetHandler(req, res, next).catch(next);
+            return;
+        }
 
-        // if (!currentUser) {
-        //   res.status(401).json({ status: "unauthorized" });
-        //   return;
-        // }
-
-        // const userCheckResult = userCheck(req);
-
-        // if (userCheckResult.type === "failure") {
-        //   res.status(403).json({ status: "forbidden", message: userCheckResult.message });
-        //   return;
-        // }
-
-        const results = await queryAsync(`SELECT * FROM ${table}`);
-        res.json(results);
+        queryAsync(`SELECT * FROM ${table}`)
+            .then(results => res.json(results))
+            .catch(next);
     });
 
-    router.post("/", async (req, res) => {
-            const addressInsertSql = `
-                INSERT INTO addresses (street, city, country, postalCode) 
-                VALUES (?, ?, ?, ?);
-            `;
-            const addressData = [req.body.street, req.body.city, req.body.country, req.body.postalCode];
-            const addressResult = await queryAsync(addressInsertSql, addressData);
+    // POST Handler, using non-async function
+    router.post("/", (req, res, next) => {
+        if (customPostHandler) {
+            customPostHandler(req, res, next).catch(next);
+            return;
+        }
 
-
-            const schoolData = {
-                ...req.body,         
-            };
-            delete schoolData.street; 
-            delete schoolData.city;
-            delete schoolData.country;
-            delete schoolData.postalCode;
-    
-            await queryAsync(`INSERT INTO ${table} SET ?`, schoolData);
-    
-            res.json({ status: "ok" });
+        // Your original POST logic integrated here
+        const addressInsertSql = `
+            INSERT INTO addresses (street, city, country, postalCode) 
+            VALUES (?, ?, ?, ?);
+        `;
+        const addressData = [req.body.street, req.body.city, req.body.country, req.body.postalCode];
+        queryAsync(addressInsertSql, addressData)
+            .then(addressResult => {
+                const schoolData = { ...req.body };
+                delete schoolData.street;
+                delete schoolData.city;
+                delete schoolData.country;
+                delete schoolData.postalCode;
+                return queryAsync(`INSERT INTO ${table} SET ?`, schoolData);
+            })
+            .then(() => res.json({ status: "ok" }))
+            .catch(next);
     });
-    
-    router.put("/:id", async (req, res) => {
-        await queryAsync(`UPDATE ${table} SET ? WHERE id = ?`, [req.body, req.params.id]);
-        res.json({ status: "ok" });
+
+    // PUT Handler, using non-async function
+    router.put("/:id", (req, res, next) => {
+        if (customPutHandler) {
+            customPutHandler(req, res, next).catch(next);
+            return;
+        }
+
+        queryAsync(`UPDATE ${table} SET ? WHERE id = ?`, [req.body, req.params.id])
+            .then(() => res.json({ status: "ok" }))
+            .catch(next);
     });
 
-    router.delete("/:id", async (req, res) => {
-        await queryAsync(`DELETE FROM ${table} WHERE id = ?`, req.params.id);
-        res.json({ status: "ok" });
+    // DELETE Handler, using non-async function
+    router.delete("/:id", (req, res, next) => {
+        if (customDeleteHandler) {
+            customDeleteHandler(req, res, next).catch(next);
+            return;
+        }
+
+        queryAsync(`DELETE FROM ${table} WHERE id = ?`, req.params.id)
+            .then(() => res.json({ status: "ok" }))
+            .catch(next);
     });
 
     return router;
